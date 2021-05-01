d2feab87454e7b5f10edc7d65873a1a0722d105e
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-04-30 19:12:37 -0600

    refactor(folder-structure): remove unused files

--- pages/auth.tsx
+++
@@ -1,8 +0,0 @@
-// import Layout from "src/components/layout";
-// import FirebaseAuth from "src/components/firebaseAuth";
-// import { GetServerSideProps, NextApiRequest } from "next";
-// import { loadIdToken } from "src/auth/firebaseAdmin";
-
-export default function Auth() {
-  return <div>Auth</div>;
-}
--- pages/houses/add.tsx
+++
@@ -1,8 +0,0 @@
-// import { GetServerSideProps, NextApiRequest } from "next";
-// import { loadIdToken } from "src/auth/firebaseAdmin";
-// import Layout from "src/components/layout";
-// import HouseForm from "src/components/houseForm";
-
-export default function Add() {
-  return <div>Add</div>;
-}
